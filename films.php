efcf1932a6646ed61df2f8556b064bfe339d35b1
Theo <brissettheo@gmail.com>
2024-05-23 11:13:45 +0200

    ajout HTML liste films

--- films.php
+++ films.php
@@ -2,23 +2,90 @@
         include("components/header.php");
     ?>
     <main>
-        <section class="liste-films">
-            <h1>Les films</h1>
-            <div class="presentation-img">
-                <img src="img/scene.png" alt="Salle de cinéma vue depuis l'extérieur">
+        <section>
+            <h1>Les films du mois</h1>
+            <div class="presentation-liste-films">
+                <p><span>MonCiné</span> propose un nouveau film toutes les semaines. Chaque film est projeté en version française sous-titrée ainsi qu'en version originale, 1 fois par semaine.</p>
+                <p>Le film reste disponible durant 4 semaines. Pour la description ainsi que les horaires, cliquez sur le film de votre choix.
             </div>
-            <div class="presentation-texte">
-                <p>Dans un ancien théâtre, le cinéma <span>MonCiné</span> reprend les codes art-déco du début du siècle, un équipement dernier cri et une programmation inédite !</p>
-                <a href="#">A propos du cinéma...</a>
-            </div>
-            <div class="presentation-img">
-                <img src="img/salle.png" alt="Salle de cinéma vue depuis l'extérieur">
+            <div class="liste-films">
+                <div class="element-liste-films">
+                    <a href="#" class="lien-film">
+                        <img src="#" alt="Aller voir les informations sur le film The Messenger">
+                        <p>The Messenger</p>
+                    </a>
+                </div>
+                <div class="element-liste-films">
+                    <a href="#" class="lien-film">
+                        <img src="#" alt="Aller voir les informations sur le film The Florida project">
+                        <p>The Florida project</p>
+                    </a>
+                </div>
+                <div class="element-liste-films">
+                    <a href="#" class="lien-film">
+                        <img src="#" alt="Aller voir les informations sur le film Zero Dark Thirty">
+                        <p>Zero Dark Thirty</p>
+                    </a>
+                </div>
+                <div class="element-liste-films">
+                    <a href="#" class="lien-film">
+                        <img src="#" alt="Aller voir les informations sur le film Shaun of the dead">
+                        <p>Shaun of the dead</p>
+                    </a>
+                </div>
+                <div class="element-liste-films">
+                    <a href="#" class="lien-film">
+                        <img src="#" alt="Aller voir les informations sur le film Take Shelter">
+                        <p>Take Shelter</p>
+                    </a>
+                </div>
+                <div class="element-liste-films">
+                    <a href="#" class="lien-film">
+                        <img src="#" alt="Aller voir les informations sur le film Miami Vice">
+                        <p>Miami Vice</p>
+                    </a>
+                </div>
+                <div class="element-liste-films">
+                    <a href="#" class="lien-film">
+                        <img src="#" alt="Aller voir les informations sur le film Insidious">
+                        <p>Insidious</p>
+                    </a>
+                </div>
+                <div class="element-liste-films">
+                    <a href="#" class="lien-film">
+                        <img src="#" alt="Aller voir les informations sur le film Elephant">
+                        <p>Elephant</p>
+                    </a>
+                </div>
+                <div class="element-liste-films">
+                    <a href="#" class="lien-film">
+                        <img src="#" alt="Aller voir les informations sur le film gerry">
+                        <p>gerry</p>
+                    </a>
+                </div>
+                <div class="element-liste-films">
+                    <a href="#" class="lien-film">
+                        <img src="#" alt="Aller voir les informations sur le film 3:10 to Yuma">
+                        <p>3:10 to Yuma</p>
+                    </a>
+                </div>
+                <div class="element-liste-films">
+                    <a href="#" class="lien-film">
+                        <img src="#" alt="Aller voir les informations sur le film Dallas Buyers Club">
+                        <p>Dallas Buyers Club</p>
+                    </a>
+                </div>
+                <div class="element-liste-films">
+                    <a href="#" class="lien-film">
+                        <img src="#" alt="Aller voir les informations sur le film Rampart">
+                        <p>Rampart</p>
+                    </a>
+                </div>
             </div>
         </section>
         <section class="decouvrir">
             <?php
-                include("components/aside_serie.php");
-                include("components/aside_films.php");
+                include("components/aside_film_semaine.php");
             ?>
         </section>
     </main>
